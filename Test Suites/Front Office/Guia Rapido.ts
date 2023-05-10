905487c78d2ffe55e7ec2378fce1ae72df861494
william.bumba <willian.bumba@tistech.co.ao>
2023-05-10 16:10:33 +0200

    suite de teste para consultar o gia rápido

---
+++ Test Suites/Front Office/Guia Rapido.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Guia Rapido</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>379f2a02-d55d-43ad-83fc-c713273df749</testSuiteGuid>
+</TestSuiteEntity>
